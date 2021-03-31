521a96be64bf6e4a64cd797820d3049c519ddbfe
delegalog <sistemas@delegalog.com>
2021-03-31 21:46:14 +0200

    m

--- dlg_crm/models/opportunity.py
+++ dlg_crm/models/opportunity.py
@@ -50,56 +50,6 @@ class Opportunity(models.Model):
         opportunity.unlink()
 
 
-class Phase(models.Model):
-    _name = 'dlg_crm.status'
-    _description = 'Phase'
-
-    id = fields.Char(string='ID')
-    name = fields.Char(string='Nombre')
-
-    def toggle_state(self):
-        self.done = not self.done
-
-    #ORM
-    def f_create(self):
-        status = {
-            'id': 'ORM test',
-            'name': 'ORM test'
-        }
-        print(status)
-        self.env['dlg_crm.phase'].create(status)
-
-    def f_search_update(self):
-        phase = self.env['dlg_crm.phase'].search([('name', '=', 'ORM test')])
-        print('search()', phase, phase.name)
-
-        phase_b = self.env['dlg_crm.phase'].browse([8])
-        print('browse()', phase_b, phase_b.name)
-
-        phase.write({
-            'name': 'ORM test write'
-        })
-
-    def f_delete(self):
-        phase = self.env['dlg_crm.phase'].browse([8])
-        phase.unlink()
-
-
-class PhaseReport(models.AbstractModel):
-
-    _name = 'report.dlg_crm.report_phase_card'
-
-    @api.model
-    def _get_report_values(self, docids, data=None):
-        report_obj = self.env['ir.actions.report']
-        report = report_obj._get_report_from_name('dlg_crm.report_phase_card')
-        return {
-            'doc_ids': docids,
-            'doc_model': self.env['dlg_crm.phase'],
-            'docs': self.env['dlg_crm.phase'].browse(docids)
-        }
-
-
 class OpportunityReport(models.AbstractModel):
 
     _name = 'report.dlg_crm.report_opportunity_card'
