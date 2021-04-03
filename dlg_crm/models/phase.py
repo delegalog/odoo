eb55ea8c0ca0a85fe365c10641b7286f02fcd0d7
delegalog <sistemas@delegalog.com>
2021-04-03 19:48:21 +0200

    Update phase.py

--- dlg_crm/models/phase.py
+++ dlg_crm/models/phase.py
@@ -10,7 +10,8 @@ class Phase(models.Model):
 
     id = fields.Integer(string='ID')
     name = fields.Char(string='Nombre')
-    opportunity = fields.One2many(opportunity.Opportunity, 'phase', string="Oportunidad")
+    #opportunity = fields.One2many(opportunity.Opportunity, 'phase', string="Oportunidad")
+    opportunity = fields.Many2one('opportunity', string="Oportuidad")
 
     #ORM
     def f_create(self):
