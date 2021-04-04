7081d4c97e62e8ba2ddb3f7acaffa192fbd060bf
delegalog <sistemas@delegalog.com>
2021-04-04 07:57:35 +0200

    l

--- dlg_crm/controllers/controllers.py
+++ dlg_crm/controllers/controllers.py
@@ -9,7 +9,7 @@ class OpportunityController(http.Controller):
     @http.route('/api/opportunity', auth='public', method=['GET'], csrf=False)
     def get_opportunity(self, **kw):
         try:
-            opportunity = http.request.env['dlg.crm.opportunity'].sudo().search_read([], ['id', 'name', 'customer',
+            opportunity = http.request.env['dlg_crm.opportunity'].sudo().search_read([], ['id', 'name', 'customer',
                                                                                           'notes', 'done', 'image',
                                                                                           'phase_id', 'done'])
             res = json.dumps(opportunity, ensure_ascii=False).encode('utf-8')
@@ -23,7 +23,7 @@ class PhaseController(http.Controller):
     @http.route('/api/phase', auth='public', method=['GET'], csrf=False)
     def get_phase(self, **kw):
         try:
-            phase = http.request.env['dlg.crm.phase'].sudo().search_read([], ['id', 'name', 'opportunity_id'])
+            phase = http.request.env['dlg_crm.phase'].sudo().search_read([], ['id', 'name', 'opportunity_id'])
             res = json.dumps(phase, ensure_ascii=False).encode('utf-8')
             return Response(res, content_type='application/json;charset=utf-8', status=200)
         except Exception as e:
