3cbe0d4e1a62574a3c476be00f7c6e32638eb13f
delegalog <sistemas@delegalog.com>
2021-04-07 19:40:59 +0200

    Update action.py

--- dlg_crm/models/action.py
+++ dlg_crm/models/action.py
@@ -18,7 +18,7 @@ class Action(models.Model):
                              ('D', 'Comida'), ('E', 'email')], string='Tipo', required=False)
     done = fields.Boolean(string='Finalizada')
     image = fields.Binary(string='Imagen')
-    opportunity = fields.Many2one('dlg_crm.opportunity', 'name', string="Oportunidad", required=False)
+    opportunity = fields.Many2one('dlg_crm.opportunity', string="Oportunidad", required=False)
     color = fields.Integer()
 
     _order = 'date_event asc'
