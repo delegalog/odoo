7c9f14b118f81d4b2505ed0ed46adb3573bfe9b9
delegalog <sistemas@delegalog.com>
2021-04-12 11:00:12 +0200

    d

--- dlg_projects/controllers/controllers.py
+++ dlg_projects/controllers/controllers.py
@@ -26,7 +26,8 @@ class PhaseController(http.Controller):
     @http.route('/api/phase', auth='public', method=['GET'], csrf=False)
     def get_phase(self, **kw):
         try:
-            phase = http.request.env['dlg_projects.phase'].sudo().search_read([], ['id', 'name'])
+            phase = http.request.env['dlg_projects.phase'].sudo().search_read([], ['id',
+                                                                                   'name'])
             res = json.dumps(phase, ensure_ascii=False).encode('utf-8')
             return Response(res, content_type='application/json;charset=utf-8', status=200)
         except Exception as e:
