19db0c405143dc96f40a3e237839f695502dc185
delegalog <sistemas@delegalog.com>
2021-06-29 13:05:17 +0200

    Revert "Merge branch 'staging' of https://github.com/delegalog/odoo into staging"
    This reverts commit 44a08a4300fec545d0033b3a90a6c65dcef6904d, reversing
    changes made to 83dd24d1c5baa3389462bd8d4719d989089f011d.

--- prestashop_connector_gt/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import models
-from . import wizard
-from . import prestapyt
-from . import prestapyt_old
-
--- prestashop_connector_gt/prestapyt_old/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import dict2xml
-from . import prestapyt
-from . import unicode_encode
-from . import version
-from . import xml2dict
-
