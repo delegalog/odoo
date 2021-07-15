b08a48f49a6cc83aa84feef25a778bfacb14a768
delegalog <sistemas@delegalog.com>
2021-07-15 12:33:02 +0200

    Update import_product.py

--- woocommerce_odoo_connector/wizard/import_product.py
+++ woocommerce_odoo_connector/wizard/import_product.py
@@ -8,11 +8,13 @@
 from dateutil.parser import parse
 
 import logging
+
 _logger = logging.getLogger(__name__)
 from odoo import api, fields, models
 from odoo.tools.translate import _
 from odoo.exceptions import UserError
 from odoo.addons.odoo_multi_channel_sale.tools import remove_tags
+
 
 class ImportWoocommerceProducts(models.TransientModel):
     _name = "import.woocommerce.products"
@@ -26,26 +28,25 @@ class ImportWoocommerceProducts(models.TransientModel):
             channel=channel.channel,
             store_id=product_data["id"],
             list_price=product_data.get("price"),
-            extra_categ_ids = self._get_category_string(woocommerce, channel, product_data['categories']),
+            extra_categ_ids=self._get_category_string(woocommerce, channel, product_data['categories']),
             weight=product_data.get("weight"),
             description_sale=remove_tags(product_data.get("short_description")),
             description_purchase=remove_tags(product_data.get("description")),
-            default_code = product_data.get("sku"),
-            qty_available = product_data.get("stock_quantity"),
+            default_code=product_data.get("sku"),
+            qty_available=product_data.get("stock_quantity"),
         )
         try:
-            feed_vals['image_url'] = product_data.get("images")[0].get('src') 
+            feed_vals['image_url'] = product_data.get("images")[0].get('src')
         except:
             pass
         return feed_vals
 
-    
     def _get_category_string(self, woocommerce, channel, categories):
         str_ids = ','.join([str(category["id"]) for category in categories])
         if str_ids:
             cat_list = woocommerce.get(f'products/categories?include={str_ids}').json()
             self._woocommerce_create_product_categories(
-                    woocommerce, channel, cat_list)
+                woocommerce, channel, cat_list)
         return str_ids
 
     def _get_product_all(self, woocommerce, channel, **kwargs):
@@ -55,25 +56,23 @@ class ImportWoocommerceProducts(models.TransientModel):
 
         products = woocommerce.get(
             'products',
-            params={
-                'page': kwargs.get('page'),
-                'per_page': kwargs.get('page_size'),
-                'order': 'asc'
-            }
+            page=kwargs.get('page'),
+            per_page=kwargs.get('page_size'),
+            order='asc'
         ).json()
         if "message" in products:
             message = "Error in getting products : {}".format(products["message"])
             _logger.info(message)
             raise UserError(message)
-        return list(map(lambda x: self._get_product_dict(woocommerce, channel,x),products))
+        return list(map(lambda x: self._get_product_dict(woocommerce, channel, x), products))
 
     def _woocommerce_create_product_categories(self, woocommerce, channel, datas):
         import_category_obj = self.env['import.woocommerce.categories'].create({
-            "channel_id":channel.id,
-            "operation":"import"
+            "channel_id": channel.id,
+            "operation": "import"
         })
         [self.env['category.feed'].create(import_category_obj._get_category_vals(data))
-            for data in datas]
+         for data in datas]
 
     def _get_feed_variants(self, woocommerce, channel, product_id, variation_ids):
         variant_list = []
@@ -81,16 +80,16 @@ class ImportWoocommerceProducts(models.TransientModel):
         image = False
         for variant_id in variation_ids:
             variant = woocommerce.get(
-                'products/'+str(product_id)+"/variations/"+str(variant_id)).json()
+                'products/' + str(product_id) + "/variations/" + str(variant_id)).json()
             if "message" in variant:
-                _logger.info("Error in getting Variants : %r",variant["message"])
+                _logger.info("Error in getting Variants : %r", variant["message"])
                 continue
             if variant['attributes']:
                 attribute_list = [{
-                    'name':attribute['name'],
-                    'value':attribute['option'],
+                    'name': attribute['name'],
+                    'value': attribute['option'],
                     'attrib_name_id': attribute["id"],
-                    'attrib_value_id':attribute['option']
+                    'attrib_value_id': attribute['option']
                 } for attribute in variant['attributes']]
                 if isinstance(variant['image'], list):
                     image = variant['images'][0]['src']
@@ -103,7 +102,7 @@ class ImportWoocommerceProducts(models.TransientModel):
                 'list_price': float(variant['price']),
                 'qty_available': variant['stock_quantity'],
                 'weight': variant.get("weight"),
-                'default_code':variant.get('sku'),
+                'default_code': variant.get('sku'),
             })
         return variant_list
 
@@ -115,7 +114,7 @@ class ImportWoocommerceProducts(models.TransientModel):
                 woocommerce, channel, product['id'], product['variations']))
         return vals
 
-    def woocommerce_create_product_feed(self, woocommerce,channel_id,product_id):
+    def woocommerce_create_product_feed(self, woocommerce, channel_id, product_id):
         vals = {}
         url = 'products/%s' % product_id
         product = woocommerce.get(url).json()
@@ -129,19 +128,19 @@ class ImportWoocommerceProducts(models.TransientModel):
             variation_ids = product['variations']
             variants = self._get_feed_variants(
                 woocommerce, channel_id, product['id'], variation_ids)
-            variants = [(0,0,variant) for variant in variants]
+            variants = [(0, 0, variant) for variant in variants]
             vals.update(feed_variants=variants)
-        
+
         match = self.channel_id.match_product_feeds(vals['store_id'])
         if not match:
             return self.env['product.feed'].create(vals)
         else:
             match.write(vals)
             return match
-    
+
     def _get_product_by_id(self, woocommerce, channel_id, **kwargs):
         product = woocommerce.get(f"products/{kwargs.get('woocommerce_object_id')}").json()
-        if "message" in product: 
+        if "message" in product:
             raise UserError(f"Error in getting products : {product['message']}")
         return [self._get_product_dict(woocommerce, channel_id, product)]
 
@@ -165,11 +164,11 @@ class ImportWoocommerceProducts(models.TransientModel):
                 'after': kwargs.get('woocommerce_import_date_from'),
                 'page': kwargs.get('page'),
                 'per_page': kwargs.get('page_size'),
-                'order':'asc' if kwargs.get("from_cron") else 'desc'
+                'order': 'asc' if kwargs.get("from_cron") else 'desc'
             }
         ).json()
         try:
-            vals_list = list(map(lambda x: self._get_product_dict(woocommerce, channel,x),products))
+            vals_list = list(map(lambda x: self._get_product_dict(woocommerce, channel, x), products))
             if kwargs.get("from_cron"):
                 channel.import_product_date = parse(products[-1].get("date_created_gmt"))
         except:
